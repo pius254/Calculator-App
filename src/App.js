92561d5d072a04f1cd3c1158e26aea459f7eef1c
Pius Muriithi <piusmuriithi1@gmail.com>
2022-09-03 11:54:34 +0300

    Implement calc operational clicks

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Wrapper from "./components/Wrapper";
 import Screen from "./components/Screen";
 import ButtonBox from "./components/ButtonBox";
 import Button from "./components/Button";
+import CalcProvider from "./context/CalcContext";
 
 const btnValues = [
   ["C", "+-", "%", "/"],
@@ -14,7 +15,7 @@ const btnValues = [
 
 function App() {
   return (
-    <div>
+    <CalcProvider>
       <Wrapper>
         <Screen />
         <ButtonBox>
@@ -23,7 +24,7 @@ function App() {
           ))}
         </ButtonBox>
       </Wrapper>
-    </div>
+    </CalcProvider>
   );
 }
 
--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, { useContext } from "react";
+import { CalcContext } from "../context/CalcContext";
+import { Textfit } from "react-textfit";
 
 const Screen = () => {
-  return <div className="screen">3973861</div>;
+  const { calc } = useContext(CalcContext);
+
+  return (
+    <Textfit className="screen" max={70} mode="single">
+      {calc.num ? calc.num : calc.res}
+    </Textfit>
+  );
 };
 
 export default Screen;
